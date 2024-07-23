a028b25a219c5cb42f82d1afc0d7d5994bb020f3
huutvpn <143250398+huutvpn@users.noreply.github.com>
2024-07-23 10:59:21 +0700

    Add files via upload

---
+++ limit/info.sh
@@ -0,0 +1,7 @@
+clear
+figlet -f 3d "BADBUD STORE TUNNELING" | lolcat
+cat /root/log-install.txt
+echo "-----------------------------------------------------"
+echo "                Credit Script @badbudstore" | lolcat
+echo "                   Author Script BADBUD STORE" | lolcat
+echo "-----------------------------------------------------"
